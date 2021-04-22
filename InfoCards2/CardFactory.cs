b4445697a843349712e4efb6f08434fb7d000690
C-Jess <cjess2405@gmail.com>
2021-04-23 00:16:27 +0100

    Credit card info card displays in create new card form

---
+++ InfoCards2/CardFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class CardFactory : IInfoCardFactory
+    {
+        Dictionary<string, string> descriptions = new Dictionary<string, string>{ {"Credit Card","Store credit card details"} };
+        string[] _categoriesSupported = {"Credit Card"};
+        public string[] CategoriesSupported
+        {
+            get
+            {
+                return _categoriesSupported;
+            }
+        }
+
+        public IInfoCard CreateInfoCard(string initialDetails)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IInfoCard CreateNewInfoCard(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDescription(string category)
+        {
+            return descriptions[category];
+        }
+    }
+}
---
+++ InfoCards2/CreditCard/CreditCardCard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment.CreditCard
+{
+    class CreditCardCard : IInfoCard
+    {
+        string _category = "Credit Card";
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string Category
+        {
+            get
+            {
+                return _category;
+            }
+        }
+
+        public void CloseDisplay()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayData(Panel displayPanel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDataAsString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
